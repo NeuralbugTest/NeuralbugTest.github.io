6b00d99b559130fbd329d1b89e019fbb6ee015eb
NeuralBug <neuralbug@AurioPinto.local>
2023-09-15 12:32:03 +0200

    pset2

---
+++ js/about.92c92a5f.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkswiper"]=self["webpackChunkswiper"]||[]).push([[443],{41272:function(n,s,t){t.r(s),t.d(s,{default:function(){return l}});var u=function(){var n=this;n._self._c;return n._m(0)},e=[function(){var n=this,s=n._self._c;return s("div",{staticClass:"about"},[s("h1",[n._v("This is an about page")])])}],r=t(1001),i={},a=(0,r.Z)(i,u,e,!1,null,null,null),l=a.exports}}]);
+//# sourceMappingURL=about.92c92a5f.js.map
